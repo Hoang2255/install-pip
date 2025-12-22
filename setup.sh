4daeb3028e0a9ae0a579b4945b4de8bbb6e5c0a7
Hoang2255 <117719276+Hoang2255@users.noreply.github.com>
2025-12-22 19:08:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -72,7 +72,7 @@ curl -fsSL https://raw.githubusercontent.com/Hoang2255/install-pip/refs/heads/ma
 progress_bar $! "Install pip"
 
 # 7. CÀI THƯ VIỆN PYTHON
-pip install socks5 fake_useragent colorama mechanize requests bs4 pystyle \
+pip install httpx fake_useragent colorama cloudscraper mechanize requests bs4 pystyle \
   > /dev/null 2>&1 &
 progress_bar $! "Python libraries"
 
